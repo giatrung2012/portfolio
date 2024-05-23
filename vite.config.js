c27423c8e7ca478e439a55a73fc9a880104698cc
Gia Trung <contact@giatrung.me>
2024-05-23 14:48:15 +0000

    chore: Update Vite configuration and npm dependencies
    This commit updates the Vite configuration in the vite.config.js file by adding the necessary imports for the "vite" and "react" plugins. It also updates the npm dependencies to their latest versions. These changes were made to ensure compatibility and improve the development environment.

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,7 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   css: {
@@ -5,5 +9,5 @@ export default defineConfig({
       localsConvention: "camelCase",
     },
   },
-  base: "/portfolio/",
+  base: "/portfolio/"
 });
